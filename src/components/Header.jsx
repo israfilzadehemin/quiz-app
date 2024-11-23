8e4457185a2f1500482dcf77f64c85951a5365ff
eminisra <israfilzadehemin@gmailcom>
2024-11-23 18:30:19 +0100

    Add hello div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import logo from "../assets/quiz-logo.png";
 export default function Header() {
   return (
     <header>
+      <div>Hello</div>
       <img src={logo} alt="Quiz Logo" />
       <h1>React Quiz</h1>
     </header>
